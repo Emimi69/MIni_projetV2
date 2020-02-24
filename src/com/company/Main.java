761c705c74d308b6cb1e4ba23a32307495acbf51
emilienmarion <emilien.marion@insa-lyon.fr>
2020-02-24 13:08:26 +0100

    Demo au tits

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,6 @@ public class Main {
 
     public static void main(String[] args) {
         gameWindows gW = new gameWindows("gameWindow", 600, 800);
-
+        System.out.println("ta mere");
     }
 }
